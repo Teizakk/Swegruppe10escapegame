1f12d484237b945e0e9811c3e1b2765446dce931
Teizakk <domenic.r.dallmann@web.de>
2016-11-14 12:19:21 +0100

    Anfang der Levelgeneration
    Angefangen Levelgeneration zu schreiben mit Hilfe des 2D Roguelike
    Tutorials. Rest folgt

---
+++ TheMightyQuestForEpicGrades/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour {
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+    private char[,] levelData;
+
+    // TODO
+    // Initialisiere die Liste mit den möglichen Positionen
+    void InitializeList()
+    {
+        
+    }
+
+    // TODO
+    // Erstelle die Wände und den Boden
+    void BoardSetup()
+    {
+
+    }
+
+    // TODO
+    // Erhalte eine zufällige Position für eine Truhe
+    Vector3 RandomPosition()
+    {
+        return new Vector3();
+    }
+
+    // TODO
+    // Setze das Objekt (Truhe) an der RandomPosition
+    void LayoutObjectAtRandom(GameObject gameObj, int min, int max)
+    {
+
+    }
+
+    // TODO
+    // Erstelle die Szene
+    public void SetupScene(int level)
+    {
+        levelData = GetComponent<LevelLoader>().loadLevel(level);
+
+        BoardSetup();
+    }
+}
---
+++ TheMightyQuestForEpicGrades/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class LevelLoader : MonoBehaviour {
+
+    public string Dateipfad = "./Level/";
+    public string Dateiprefix = "Level_";
+    public string Dateiendung = ".txt";
+
+    private string Dateiname;
+
+    public char[,] loadLevel(int level)
+    {
+        string Dateiname = Dateiprefix + level + Dateiendung;
+        List<string> levelData = new List<string>();
+        levelData.Clear();
+
+        using (StreamReader Datei = new StreamReader(Dateipfad + Dateiname))
+        {
+            string data;
+
+            while ((data = Datei.ReadLine()) != null)
+            {
+                levelData.Add(data);
+            }
+        }
+
+        return to2dCharArray(levelData);
+    }
+
+    char[,] to2dCharArray(List<string> list)
+    {
+        char[,] charArray2d = new char[list.Count, list[0].Length];
+
+        for (int i = 0; i < list.Count; ++i)
+            for (int j = 0; j < list[i].Length; ++j)
+                charArray2d[i,j] = list[i][j];
+
+        return charArray2d;
+    }
+
+    void Start()
+    {
+
+    }
+}
