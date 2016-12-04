0ff2f8accbf5a4e96ccf59eb10b37994e5860e28
lenny92 <lennart.swalve@mail.de>
2016-12-04 09:19:47 +0100

    Portale

---
+++ TheMightyQuestForEpicGrades/Assets/Scripts/BluePortalSkript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BluePortalSkript : MonoBehaviour {
+
+    public static bool activated;
+    public int levelIndex;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && GreenPortalSkript.activated == true && PinkPortalSkript.activated == true)
+        {
+            
+            SceneManager.LoadScene(levelIndex);
+        }
+    }
+
+    
+}
