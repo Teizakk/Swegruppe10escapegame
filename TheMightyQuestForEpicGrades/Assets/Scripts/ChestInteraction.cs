63d618707b758ffc711f3b1ebd5e1aa9b69b62d6
juergenbernhardt <juergenbernhardt@gmx.de>
2016-12-07 15:09:59 +0100

    ChestInteraction

---
+++ TheMightyQuestForEpicGrades/Assets/Scripts/ChestInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ChestInteraction : MonoBehaviour
+{
+    private bool ChestIsOpen = false;
+    private bool ChestIsLocked = false;
+
+    public void OnTriggerStay2D(Collider2D col)
+    {
+        if (Input.GetKeyDown("e") && !ChestIsLocked) //col.gameObject.tag == "Player")
+        {
+            ChestIsOpen = true;
+            OpenChest();
+            ChestIsLocked = true; //Truhe abschließen
+            Destroy(gameObject);//Lässt Truhe verschwinden
+        }
+    }
+
+    public void OpenChest()
+    {
+        
+        //Fragemenü aufrufen
+        ChestIsOpen = false;
+    }
+}
