be685085d2b14a1144764185b0bad46de044fb83
lenny92 <lennart.swalve@mail.de>
2016-12-12 20:16:40 +0100

    cheatmode
    true false richtig gemacht

--- TheMightyQuestForEpicGrades/Assets/Scripts/cheatmode.cs
+++ TheMightyQuestForEpicGrades/Assets/Scripts/cheatmode.cs
@@ -15,39 +15,8 @@ public class cheatmode : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-     /*   if (Input.GetKeyDown("h"))
-        {
-            Debug.Log("h");
-            h = true;
-            
+     
 
-
-        }
-
-        if (Input.GetKeyDown("a"))
-        {
-            if (h == true)
-            {
-                Debug.Log("a");
-                a = true;
-            }
-
-
-        }
-
-        if (Input.GetKeyDown("x"))
-        {
-            if (h == true && a == true)
-            {
-                Debug.Log("x");
-                GameManager.cheat = true;
-                if (GameManager.cheat == true)
-                    Debug.Log("easy");
-            }
-
-    
-
-        }*/
         if (Input.anyKey) {
 
             if (Input.GetKey("h"))
@@ -69,7 +38,7 @@ public class cheatmode : MonoBehaviour {
             {
                 Debug.Log("false");
                 h = false;
-                a = true;
+                a = false;
             }
 
         }
