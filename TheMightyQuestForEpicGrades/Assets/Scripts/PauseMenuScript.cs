67f1e1772d190164a213548f77599fffbf7b54af
Tobi <opel.tobias@gmx.de>
2016-12-16 17:22:05 +0100

    FEAT: TO-Pausenmenu V1 ohne Save + Load
    Pausenmenü bis auf Save und Load Funktionen fertig gestellt.
    
    Optik lässt allerdings teilweise noch zu wünschen übrig...

--- TheMightyQuestForEpicGrades/Assets/Scripts/PauseMenuScript.cs
+++ TheMightyQuestForEpicGrades/Assets/Scripts/PauseMenuScript.cs
@@ -1,92 +1,160 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
 public class PauseMenuScript : MonoBehaviour {
 
-    //private bool switches
-    private bool gamePaused;
-    private bool visible;
-    private bool fadingIn;
-    private bool fadingOut;
+    private const float _FADE_FACTOR = 0.04f;
 
-    #region Submenus
+    //private bool switches
+    private bool _gamePaused;
+    private bool _visible;
+    private bool _fadingIn;
+    private bool _fadingOut;
+    private bool _inSubWindow;
+
+    [Header("Untermenüs")]
     public GameObject SaveMenu;
     public GameObject LoadMenu;
     public GameObject CloseWarning;
-    #endregion
 
-    //private CanvasGroup pauseMenuCanvasGroup;
-    //private CanvasGroup saveMenuCanvasGroup;
-    //private CanvasGroup loadMenuCanvasGroup;
-    //private CanvasGroup closeWarningCanvasGroup;
+    [Header("Pausenmenü Buttons")]
+    public Button SpielFortsetzenButton;
+    public Button SpielSpeichernButton;
+    public Button SpielLadenButton;
+    public Button ZurückZumHauptmenüButton;
+
+    private CanvasGroup _pauseMenuCanvasGroup;
+    private CanvasGroup _saveMenuCanvasGroup;
+    private CanvasGroup _loadMenuCanvasGroup;
+    private CanvasGroup _closeWarningCanvasGroup;
 
     //Funktion um von außen zu fragen, ob das Spiel pausiert ist (für die Controls zb)
     public bool IsGamePaused() {
-        return gamePaused;
+        return _gamePaused;
+    }
+    //"Nachrichten"-Funktion an PlayerController
+    public void blockAndUnblockMovement() {
+        PlayerController.instance.switchControlBlock();
     }
 
     // Use this for initialization
     void Start () {
+        //Das Pausenmenü selbst
 	    gameObject.SetActive(true);
-        gameObject.GetComponent<CanvasGroup>().alpha = 0;
+        _pauseMenuCanvasGroup = gameObject.GetComponent<CanvasGroup>();
+        _pauseMenuCanvasGroup.alpha = 0;
+        _pauseMenuCanvasGroup.interactable = false;
+        //Das Speichernmenü
         SaveMenu.SetActive(false);
+        _saveMenuCanvasGroup = SaveMenu.GetComponent<CanvasGroup>();
+        _saveMenuCanvasGroup.alpha = 0;
+        _saveMenuCanvasGroup.interactable = false;
+        //Das Lademenü
         LoadMenu.SetActive(false);
+        _loadMenuCanvasGroup = LoadMenu.GetComponent<CanvasGroup>();
+        _loadMenuCanvasGroup.alpha = 0;
+        _loadMenuCanvasGroup.interactable = false;
+        //Die Schließen-Warnung
         CloseWarning.SetActive(false);
-	}
-    private void StartFadingIn() {
-        //TODO
+        _closeWarningCanvasGroup = CloseWarning.GetComponent<CanvasGroup>();
+        _closeWarningCanvasGroup.alpha = 0;
+        _closeWarningCanvasGroup.interactable = false;
     }
 
-    private void StartFadingOut() {
-        //TODO
+    #region show&hide submenus
+    //Speichermenü anzeigen / ausblenden
+    public void ShowSaveMenu() {
+        SaveMenu.SetActive(true);
+        _saveMenuCanvasGroup.alpha = 1.0f;
+        _saveMenuCanvasGroup.interactable = true;
+        _inSubWindow = true;
     }
+    public void HideSaveMenu() {
+        _saveMenuCanvasGroup.alpha = 0.0f;
+        _saveMenuCanvasGroup.interactable = false;
+        _inSubWindow = false;
+        SaveMenu.SetActive(false);
+    }
+    //Lademenü anzeigen / ausblenden
+    public void ShowLoadMenu() {
+        LoadMenu.SetActive(true);
+        _loadMenuCanvasGroup.alpha = 1.0f;
+        _loadMenuCanvasGroup.interactable = true;
+        _inSubWindow = true;
+    }
+    public void HideLoadMenu() {
+        _loadMenuCanvasGroup.alpha = 0.0f;
+        _loadMenuCanvasGroup.interactable = false;
+        _inSubWindow = false;
+        LoadMenu.SetActive(false);
+    }
+    //Schließwarnung anzeigen / ausblenden
+    public void ShowCloseWarning() {
+        CloseWarning.SetActive(true);
+        _closeWarningCanvasGroup.alpha = 1.0f;
+        _closeWarningCanvasGroup.interactable = true;
+        _inSubWindow = true;
+    }
+    public void HideCloseWarning() {
+        _closeWarningCanvasGroup.alpha = 0.0f;
+        _closeWarningCanvasGroup.interactable = false;
+        _inSubWindow = false;
+        CloseWarning.SetActive(false);
+    }
+    #endregion
+
+    public void ContinueGame() {
+        _fadingOut = true;
+    }
+
     // Update is called once per frame
     void Update () {
         //Wenn ESC gedrückt wurde und es noch nicht gerade erscheint oder verschwindet
-	    if (Input.GetKeyDown(KeyCode.Escape) && !fadingIn && !fadingOut) {
+	    if (Input.GetKeyDown(KeyCode.Escape) && !_fadingIn && !_fadingOut && !_inSubWindow) {
             //Und das Pausenmenu nicht angezeigt wird
-            if (!visible) { 
-	            fadingIn = true;
-                StartFadingIn();
+            if (!_visible) { 
+	            _fadingIn = true;
 	        }
             //Oder gerade angezeigt wird
-            else if (visible) {
-	            fadingOut = true;
-	            StartFadingOut();
+            else if (_visible) {
+	            _fadingOut = true;
 	        }
 	    }
         //Wenn es gerade dabei ist zu erscheinen oder zu verschwinden
-	    else if (fadingIn || fadingOut) {
+	    else if (_fadingIn || _fadingOut) {
             var mainCanvasGroup = GetComponent<CanvasGroup>();
             //Wenn wir gerade einblenden
-            if (fadingIn) {
+            if (_fadingIn) {
                 //Wenn es die volle Sichtbarkeit erreicht hat
                 if (mainCanvasGroup.alpha.Equals(1.0f)) { //... sonst Verlust der Genauigkeit? ist das hässlich...
-                    visible = true;
+                    _visible = true;
                     mainCanvasGroup.interactable = true;
-                    fadingIn = false;
-                    gamePaused = true;
+                    _fadingIn = false;
+                    _gamePaused = true;
+                    blockAndUnblockMovement();
                     Debug.Log("Spiel ist nun pausiert");
                 }
                 //Wenn es noch nicht voll sichtbar ist
                 else {
-                    GetComponent<CanvasGroup>().alpha += 0.02f; //50 Ticks für volles alpha
+                    GetComponent<CanvasGroup>().alpha += _FADE_FACTOR;
                 }
 	        }
             //Wenn wir gerade ausblenden
 	        else {
                 //Wenn es komplett unsichtbar geworden ist
                 if (mainCanvasGroup.alpha.Equals(0.0f)) {
-                    visible = false;
+                    _visible = false;
                     mainCanvasGroup.interactable = false;
-                    fadingOut = false;
-                    gamePaused = false;
+                    _fadingOut = false;
+                    _gamePaused = false;
+                    blockAndUnblockMovement();
                     Debug.Log("Spiel wird fortgesetzt");
                 }
                 //Wenn es noch nicht voll sichtbar ist
                 else {
-                    GetComponent<CanvasGroup>().alpha -= 0.02f; //50 Ticks für komplett unsichtbar
+                    GetComponent<CanvasGroup>().alpha -= _FADE_FACTOR;
                 }
             }
         }
